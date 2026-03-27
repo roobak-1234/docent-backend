3b9d53f87780a3fa025ce7359236fe78d97adb7c
roobak <roobakdinesh@gmail.com>
2026-03-27 19:46:25 +0530

    commit-007

--- obj/Debug/net8.0/WebDashboardBackend.AssemblyInfo.cs
+++ obj/Debug/net8.0/WebDashboardBackend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebDashboardBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a9a5724c181b1fedf2d4aecf9434d17e5f14971")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+65d3f1beedb5c787316a8d1d1ffeba551fa701ea")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebDashboardBackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebDashboardBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
